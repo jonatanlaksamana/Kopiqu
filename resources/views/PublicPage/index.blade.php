2afdc48d8acac2fdbfff2ce7ff8ff36edd58b0fa
Jonatan Laksamana <jonatanlaksamanapurnomo@gmail.com>
2019-03-14 05:54:35 +0700

    fixing ui for heroku

--- resources/views/PublicPage/index.blade.php
+++ resources/views/PublicPage/index.blade.php
@@ -17,10 +17,10 @@
         <div id="ensign-nivoslider" class="slides">
 
 
-            @foreach($sliders as $slide)
+            {{--@foreach($sliders as $slide)--}}
                 {{--<img src="{{asset('/storage/img/slider/' . $slide->image)}}" title="#{{$slide->title}}">--}}
-                <img src="{{secure_asset('/img/slider/' . $slide->image)}}" title="#{{$slide->title}}">
-            @endforeach
+                <img src="{{secure_asset('/img/slider/' . $sliders[0]->image)}}" title="#{{$sliders[0]->title}}">
+            {{--@endforeach--}}
 
 
         </div>
