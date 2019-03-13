1d27d9413bf3cfb46acaed9c231341b2d348d7cc
Jonatan Laksamana <jonatanlaksamanapurnomo@gmail.com>
2019-03-14 04:44:34 +0700

    fix ui

--- resources/views/PublicPage/index.blade.php
+++ resources/views/PublicPage/index.blade.php
@@ -18,7 +18,7 @@
             flassh message
 
             @foreach($sliders as $slide)
-                {{--<img src="{{asset('/storage/img/slider/' . $slide->image)}}" title="#{{$slide->title}}">--}}
+                <img src="{{asset('/storage/img/slider/' . $slide->image)}}" title="#{{$slide->title}}">
                 <img src="{{secure_asset('/img/slider/' . $slide->image)}}" title="#{{$slide->title}}">
             @endforeach
 
@@ -27,35 +27,36 @@
 
         <!-- direction 1 -->
 
-        {{--@foreach($sliders as $slide)--}}
-            {{--<div id="{{$slide->title}}" class="slider-direction">--}}
-                {{--<div class="container">--}}
-                    {{--<div class="row">--}}
-                        {{--<div class="col-md-12 col-sm-12 col-xs-12">--}}
-                            {{--<div class="slider-content">--}}
-                                {{--<!-- layer 1 -->--}}
-                                {{--<div class="layer-1-1 hidden-xs wow slideInDown" data-wow-duration="2s" data-wow-delay=".2s">--}}
-                                    {{--<h2 class="title1">{{$slide->author}}</h2>--}}
-                                {{--</div>--}}
-                                {{--<!-- layer 2 -->--}}
-                                {{--<div class="layer-1-2 wow slideInUp" data-wow-duration="2s" data-wow-delay=".1s">--}}
-                                    {{--<h1 class="title2">{{$slide->quote}}</h1>--}}
-                                {{--</div>--}}
-                                {{--<!-- layer 3 -->--}}
-                                {{--<div class="layer-1-3 hidden-xs wow slideInUp" data-wow-duration="2s" data-wow-delay=".2s">--}}
-                                    {{--<a class="ready-btn right-btn page-scroll" href="{{route('order.view')}}">Order Now</a>--}}
+        @foreach($sliders as $slide)
+            <div id="{{$slide->title}}" class="slider-direction">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12 col-sm-12 col-xs-12">
+                            <div class="slider-content">
+                                <!-- layer 1 -->
+                                <div class="layer-1-1 hidden-xs wow slideInDown" data-wow-duration="2s" data-wow-delay=".2s">
+                                    <h2 class="title1">{{$slide->author}}</h2>
+                                </div>
+                                <!-- layer 2 -->
+                                <div class="layer-1-2 wow slideInUp" data-wow-duration="2s" data-wow-delay=".1s">
+                                    <h1 class="title2">{{$slide->quote}}</h1>
+                                </div>
+                                <!-- layer 3 -->
+                                <div class="layer-1-3 hidden-xs wow slideInUp" data-wow-duration="2s" data-wow-delay=".2s">
+                                    <a class="ready-btn right-btn page-scroll" href="{{route('order.view')}}">Order Now</a>
 
-                                {{--</div>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-
-
-        {{--@endforeach--}}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
 
+        @endforeach
+
+
+</div>
 </div>
 <!-- End Slider Area -->
 
